68193f8f4945b72a3bbb99acf50d54eb013276a4
amonchik8 <amonchik8@gmail.com>
2021-07-23 22:41:50 +0300

    fix file name

--- project/src/index.js
+++ project/src/index.js
@@ -9,14 +9,14 @@ import { redirect } from './store/middlewares/redirect';
 import { requiredAuthorization } from './store/action';
 import { checkAuth, fetchOfferList } from './store/api-actions';
 import App from './components/app';
-import {reducer} from './store/reducer';
+import rootReducer from './store/root-reducer';
 import { AuthorizationStatus } from './const';
 
 const api = createAPI(() =>
   store.dispatch(
     requiredAuthorization(AuthorizationStatus.NO_AUTH)));
 const store = createStore(
-  reducer,
+  rootReducer,
   composeWithDevTools(
     applyMiddleware(thunk.withExtraArgument(api)),
     applyMiddleware(redirect)));
