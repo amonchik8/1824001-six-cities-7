9be0e862907f0dea8d09c7da0f5e3d6bd9615d16
amonchik8 <amonchik8@gmail.com>
2021-07-26 23:51:26 +0300

    Fixed bug with arrays, added memo to map

--- project/src/components/common/map/map.jsx
+++ project/src/components/common/map/map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, memo } from 'react';
 import PropTypes from 'prop-types';
 import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -73,4 +73,4 @@ Map.propTypes = {
   }).isRequired,
 };
 
-export default Map;
+export default memo(Map);
